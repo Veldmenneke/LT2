4911791775e0558ca9040023de47360c7b3db2dc
Veldmenneke <niels8v@hotmail.com>
2019-03-22 12:55:53 +0100

    Startup

---
+++ aanmelden.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+<title>Aanmelden</title>
+</head>
+<body>
+<form method="post" action="proces.php">
+
+E-mail: <input type="text" name="email"><br />
+<p>
+<input type="submit" name="verstuur" value="submit" />
+</body>
+</html>
---
+++ proces.php
@@ -0,0 +1,8 @@
+<?php
+if($_POST('email')) {
+    echo "U bent aangemeld!";
+}
+else{
+    echo "Vul een geldig E-mail adres in!";
+}
+?>
